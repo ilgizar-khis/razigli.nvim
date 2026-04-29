df6cd1c8f36a4fb67dc883aba8b754f379142fc6
razigli <razigli@arch.dev>
2026-04-29 16:15:39 +0500

    fix error with keymaps

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -6,10 +6,9 @@ vim.pack.add({
 local BlinkCmp = require("blink.cmp")
 BlinkCmp.build():wait(60000)
 BlinkCmp.setup({
-
 	keymap = {
-		["<S-Tab>"] = { "select_prev", "fallback_to_mapping" },
-		["<Tab>"] = { "select_next", "fallback_to_mapping" },
-		["<CR>"] = { "select_and_accept", "fallback_to_mapping" },
+		["<S-Tab>"] = { "select_prev", "fallback" },
+		["<Tab>"] = { "select_next", "fallback" },
+		["<CR>"] = { "select_and_accept", "fallback" },
 	},
 })
