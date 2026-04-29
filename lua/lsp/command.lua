443de3be21c7c59c275f2219d2a4f8c2ce05972b
razigli <razigli@arch.dev>
2026-04-29 18:26:14 +0500

    add LspLog to config

--- lua/lsp/command.lua
+++ lua/lsp/command.lua
@@ -1,3 +1,30 @@
 vim.api.nvim_create_user_command("LspDiagnostic", function()
 	vim.diagnostic.open_float()
-end)
+end, {})
+
+vim.api.nvim_create_user_command("LspLog", function()
+	local buffer = vim.api.nvim_create_buf(false, true)
+	local width = 160
+	local height = 40
+	local col = math.floor((vim.api.nvim_get_option("columns") - width) / 2)
+	local row = math.floor((vim.api.nvim_get_option("lines") - height) / 2)
+
+	local win = vim.api.nvim_open_win(buffer, true, {
+		relative = "editor",
+		width = width,
+		height = height,
+		col = col,
+		row = row,
+		border = "single",
+	})
+
+	vim.api.nvim_win_set_option(win, "number", false)
+	vim.api.nvim_win_set_option(win, "relativenumber", false)
+
+	local home = os.getenv("HOME")
+	local log = vim.fn.readfile(home .. "/.local/state/nvim/lsp.log")
+	vim.api.nvim_buf_set_lines(buffer, 0, -1, false, log)
+	vim.api.nvim_buf_set_option(buffer, "readonly", true)
+	vim.api.nvim_buf_set_option(buffer, "modifiable", false)
+	vim.cmd.normal("G")
+end, {})
