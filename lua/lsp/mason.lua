27e6f386abc13f2e668a050438759fc52f353b8f
Ваше Имя <you@example.com>
2026-04-29 14:59:18 +0500

    edit mason.lua

---
+++ lua/lsp/mason.lua
@@ -0,0 +1,9 @@
+vim.pack.add({
+    "https://github.com/mason-org/mason.nvim.git",
+    "https://github.com/mason-org/mason-lspconfig.nvim.git",
+}
+
+require("mason").setup({})
+require("mason-lspconfig").setup({
+    ensure_installed = { "lua_ls", "pyright" }
+})
