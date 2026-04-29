015024d260a9375caca9413707d1b6218f0cb2f2
Ваше Имя <you@example.com>
2026-04-29 14:23:56 +0500

    edit ./init.lua and add base parametrs

---
+++ init.lua
@@ -0,0 +1,20 @@
+-- add nulberline
+vim.opt.number = true
+vim.opt.relativenumber = true
+-- add signscolumn
+vim.opt.signcolumn = "yes"
+-- search
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+-- indents
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 4
+vim.opt.sidescrolloff = 4
+vim.opt.termguicolors = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
