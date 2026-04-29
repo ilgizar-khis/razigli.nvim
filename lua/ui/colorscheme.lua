af0fc9e345e6f95dec4bddec31e2193debc0946a
razigli <razigli@arch.dev>
2026-04-29 18:40:33 +0500

    add nvim_sel_hl

--- lua/ui/colorscheme.lua
+++ lua/ui/colorscheme.lua
@@ -1,4 +1,11 @@
 vim.pack.add({ "https://github.com/ellisonleao/gruvbox.nvim.git" })
 
-require("gruvbox").setup({})
+require("gruvbox").setup({
+	transparent_mode = true,
+})
 vim.cmd.colorscheme("gruvbox")
+
+vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+vim.api.nvim_set_hl(0, "TabLine", { bg = "none" })
+vim.api.nvim_set_hl(0, "TabLineSel", { bg = "none" })
+vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
