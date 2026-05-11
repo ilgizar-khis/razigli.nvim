c9557cc13df5d2fcf7e1f995bad46ebe7cbc2b9a
razigli <razigli@arch.dev>
2026-05-11 18:23:18 +0500

    set colorscheme

--- lua/ui/colorscheme.lua
+++ lua/ui/colorscheme.lua
@@ -3,10 +3,11 @@ vim.pack.add({ "https://github.com/ellisonleao/gruvbox.nvim.git" })
 require("gruvbox").setup({
 	transparent_mode = true,
 })
-vim.cmd.colorscheme("gruvbox")
+vim.cmd.colorscheme("retrobox")
 
 vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
 vim.api.nvim_set_hl(0, "TabLine", { bg = "none" })
 vim.api.nvim_set_hl(0, "TabLineSel", { bg = "none" })
 vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#111111" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
