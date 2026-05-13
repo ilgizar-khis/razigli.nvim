15161d322d9a1b7ecf0d260ba59b7ed257562a16
razigli <razigli@arch.dev>
2026-05-13 14:57:40 +0500

    edit statusline.lua

--- lua/ui/statusline.lua
+++ lua/ui/statusline.lua
@@ -1,4 +1,4 @@
-vim.pack.add({"https://github.com/nvim-mini/mini.statusline.git"})
+vim.pack.add({ "https://github.com/nvim-mini/mini.statusline.git" })
 
 local MiniStatusline = require("mini.statusline")
 
@@ -7,7 +7,7 @@ MiniStatusline.setup({})
 local normal_hl = { bg = "none", fg = "#ffffff", bold = true }
 local insert_hl = { bg = "none", fg = "#ffccaa", bold = true }
 local visual_hl = { bg = "none", fg = "#aaccff", bold = true }
-local other_hl = { bg = "none", fg = "#888899" }
+local other_hl = { bg = "none", fg = "#f0fcf0" }
 local command_hl = { bg = "none", fg = "#ccffaa", bold = true }
 local replace_hl = { bg = "none", fg = "#ffffaa", bold = true }
 
