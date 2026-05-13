b7b2175aebdfe4aed71648350a2589f8c1b0d537
razigli <razigli@arch.dev>
2026-05-13 15:59:50 +0500

    configurate colorizer

--- lua/ui/colorizer.lua
+++ lua/ui/colorizer.lua
@@ -1,3 +1,13 @@
 vim.pack.add({ "https://github.com/catgoose/nvim-colorizer.lua.git" })
 
-require("colorizer").setup()
+require("colorizer").setup({
+	options = {
+		display = {
+			mode = "virtualtext",
+			virtualtext = {
+				position = "after",
+				hl_mode = "foreground",
+			},
+		},
+	},
+})
