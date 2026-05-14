5026e54156ff55556d3f635a28c9f388922a3e24
razigli <razigli@arch.dev>
2026-05-14 21:05:11 +0500

    set LspDiagnosticSigns

--- lua/lsp/lsp.lua
+++ lua/lsp/lsp.lua
@@ -1,6 +1,15 @@
-vim.pack.add({"https://github.com/neovim/nvim-lspconfig.git"})
+vim.pack.add({ "https://github.com/neovim/nvim-lspconfig.git" })
 
 vim.lsp.enable("pyright")
 vim.lsp.enable("lua_ls")
 
-
+vim.diagnostic.config({
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "X",
+			[vim.diagnostic.severity.WARN] = "!",
+			[vim.diagnostic.severity.HINT] = "?",
+			[vim.diagnostic.severity.INFO] = "I",
+		},
+	},
+})
