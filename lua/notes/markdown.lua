b4935a9d474f26e073bbfb6c4c4eb56c9ce2278f
razigli <razigli@arch.dev>
2026-05-14 20:23:22 +0500

    add keymap to markdown

--- lua/notes/markdown.lua
+++ lua/notes/markdown.lua
@@ -1,1 +1,8 @@
 vim.pack.add({ "https://github.com/OXY2DEV/markview.nvim.git" })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "markdown" },
+	callback = function()
+		vim.keymap.set("n", "\\", "<CMD>Markview splitToggle<CR>", { buffer = true })
+	end,
+})
