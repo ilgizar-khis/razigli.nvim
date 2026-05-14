293f5b9c49b5548a51f83c2af48fd4f6a03b5732
razigli <razigli@arch.dev>
2026-05-14 20:37:20 +0500

    install toggleterm

--- lua/ui/terminal.lua
+++ lua/ui/terminal.lua
@@ -1,33 +1,8 @@
 vim.pack.add({
-	{ src = "https://github.com/waiting-for-dev/ergoterm.nvim.git" },
-})
-local ergoterm = require("ergoterm")
-ergoterm.setup({
-	terminal_defaults = {
-		layout = "float",
-		float_opts = {
-			height = 40,
-			width = 160,
-			border = "single",
-			title = "",
-			title_pos = "center",
-		},
-		float_winblend = 0,
-		cleanup_on_success = false,
-		auto_scroll = true,
-	},
+	{ src = "https://github.com/akinsho/toggleterm.nvim.git" },
 })
 
-vim.keymap.set("n", "tt", function()
-	term = ergoterm.get_all()
-	if term[1] == nil then
-		vim.cmd("TermNew<CR>")
-	else
-		term[1]:focus()
-	end
-end)
+require("toggleterm").setup({})
 
-vim.keymap.set("t", "<ESC>", function()
-	local term = ergoterm.get_focused()
-	term:unfocus()
-end)
+vim.keymap.set("n", "tt", "<CMD>ToggleTerm<CR>")
+vim.keymap.set("t", "<ESC>", "<CMD>ToggleTerm<CR>")
