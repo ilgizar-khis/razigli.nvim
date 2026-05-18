5e2c75443ee8605152a94030a3d2b61150ee87b4
razigli <razigli@arch.dev>
2026-05-18 20:54:48 +0500

    set HL of toggleterm

--- lua/ui/terminal.lua
+++ lua/ui/terminal.lua
@@ -2,8 +2,13 @@ vim.pack.add({
 	{ src = "https://github.com/akinsho/toggleterm.nvim.git" },
 })
 
-require("toggleterm").setup({})
+require("toggleterm").setup({
+	highlights = {
+		NormalFloat = {
+			link = "NormalFloat",
+		},
+	},
+})
 
 vim.keymap.set("n", "tt", "<CMD>ToggleTerm direction=float<CR>", { desc = "open terminal" })
 vim.keymap.set("t", "<ESC>", "<CMD>ToggleTerm direction=float<CR>", { desc = "close terminal" })
-vim.keymap.set("t", "<TAB>", "<CMD>wincmd w<CR>", { desc = "close terminal" })
