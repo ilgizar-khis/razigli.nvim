8208775d476757d3d38842471b5f9a058bac9088
razigli <razigli@arch.dev>
2026-06-09 22:02:26 +0500

    add Pmenu colorscheme

--- init.lua
+++ init.lua
@@ -16,19 +16,27 @@ vim.opt.laststatus = 3
 -- basic
 vim.cmd("colorscheme sorbet")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#000000", fg = "#ffffff" })
+vim.api.nvim_set_hl(0, "WinSeparator", { bg = "#000000", fg = "#ffffff" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#121212" })
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#000000", fg = "#ffffff" })
-vim.api.nvim_set_hl(0, "WinSeparator", { bg = "#000000", fg = "#ffffff" })
-vim.api.nvim_set_hl(0, "SignColumn", { bg = "#000000" })
+-- lines
 vim.api.nvim_set_hl(0, "TabLine", { bg = "#000000" })
 vim.api.nvim_set_hl(0, "TabLineFill", { bg = "#000000" })
 vim.api.nvim_set_hl(0, "TabLineSel", { bg = "#000000" })
 vim.api.nvim_set_hl(0, "StatusLine", { bg = "#000000" })
--- numberlines and signcolumn
+-- cols
 vim.api.nvim_set_hl(0, "LineNr", { fg = "#ffffff", bg = "none" })
 vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#ffcabc", bg = "none" })
 vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#ffcabc", bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "#000000" })
+-- popup menu
+vim.api.nvim_set_hl(0, "Pmenu", { bg = "#121212", fg = "#ffffff" })
+vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#454545", fg = "#ffffff" })
+vim.api.nvim_set_hl(0, "PmenuKind", { bg = "#121212", fg = "#ffffff" })
+vim.api.nvim_set_hl(0, "PmenuKindSel", { bg = "#454545", fg = "#ffffff" })
+vim.api.nvim_set_hl(0, "PmenuThumbExtra", { bg = "#121212", fg = "#ffffff" })
+vim.api.nvim_set_hl(0, "PmenuThumbExtraSel", { bg = "#454545", fg = "#ffffff" })
+vim.api.nvim_set_hl(0, "PmenuThumb", { bg = "#ffffff" })
 
 -- indents config
 vim.opt.tabstop = 4
