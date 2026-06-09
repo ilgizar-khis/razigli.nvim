d51d4eefa7b153e1460e93b06e2e5b054513d349
razigli <razigli@arch.dev>
2026-06-10 00:28:06 +0500

    add hl

--- lua/git/neogit.lua
+++ lua/git/neogit.lua
@@ -9,3 +9,12 @@ require("neogit").setup({
 		underline = true,
 	},
 })
+
+-- hl to diffview
+vim.api.nvim_set_hl(0, "NeogitDiffAddHighlight", { bg = "#000000", fg = "#abffab", bold = true })
+vim.api.nvim_set_hl(0, "NeogitDiffDeleteHighlight", { bg = "#000000", fg = "#ffabab", bold = true })
+vim.api.nvim_set_hl(0, "NeogitDiffAddInline", { bg = "#008900", fg = "#ffffff", bold = true })
+vim.api.nvim_set_hl(0, "NeogitDiffDeleteInline", { bg = "#890000", fg = "#ffffff", bold = true })
+-- hl to commit buffer
+vim.api.nvim_set_hl(0, "NeogitDiffAdd", { bg = "#000000", fg = "#abffab", bold = true })
+vim.api.nvim_set_hl(0, "NeogitDiffDelete", { bg = "#000000", fg = "#ffabab", bold = true })
