2895e47919234ef678b1d9f093ac3b2faeac4a39
razigli <razigli@arch.dev>
2026-06-09 13:53:42 +0500

    add indents config

--- init.lua
+++ init.lua
@@ -2,7 +2,14 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes:2"
+
 vim.api.nvim_set_hl(0, "LineNr", {fg = "#ffffff"})
 vim.api.nvim_set_hl(0, "LineNrAbove", {fg = "#ffcabc"})
 vim.api.nvim_set_hl(0, "LineNrBelow", {fg = "#ffcabc"})
 
+-- indents config
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.smartindent = true
