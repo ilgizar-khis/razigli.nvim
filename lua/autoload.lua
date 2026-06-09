e8fd01eff3e0709d857c9dff247a6ff1455ab3ab
razigli <razigli@arch.dev>
2026-06-09 14:39:31 +0500

    add M.scan function

--- lua/autoload.lua
+++ lua/autoload.lua
@@ -3,5 +3,25 @@ local M = {}
 M.requires = {}
 M.preloads = {}
 M.postloads = {}
+M.ignores = {"autoload"}
+
+function M.scan(dirName)
+	local files = vim.fn.readdir(dirName)
+
+	for _, file in ipairs(files) do
+		local path = dirName .. "/" .. file
+		local stat = vim.uv.fs_stat(path)
+
+		if stat.type == "directory" then
+			M.scan(path, reqName)
+		else
+			if string.find(path, "%.lua") then
+				table.insert(M.requires, path)
+				vim.notify(path)
+			end
+		end
+
+	end
+end
 
 return M
