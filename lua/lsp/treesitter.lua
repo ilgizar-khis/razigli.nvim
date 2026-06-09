8d0c25a6e6f9cfc77c23416e8cccd59cf792d563
razigli <razigli@arch.dev>
2026-06-09 18:04:46 +0500

    install and config treesitter

---
+++ lua/lsp/treesitter.lua
@@ -0,0 +1,8 @@
+vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter.git" })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "python", "lua", "json" },
+	callback = function()
+		vim.treesitter.start()
+	end,
+})
