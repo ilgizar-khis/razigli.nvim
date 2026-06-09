ce52b8f01758b3e582874ac7698e3e911788f7fe
razigli <razigli@arch.dev>
2026-06-09 23:43:43 +0500

    install plugins to work with brackets

---
+++ lua/utils/brackets.lua
@@ -0,0 +1,23 @@
+vim.pack.add({
+	"https://github.com/nvim-mini/mini.pairs.git",
+	"https://github.com/kylechui/nvim-surround.git",
+})
+
+require("mini.pairs").setup({
+	modes = { insert = true, command = false, terminal = false },
+	mappings = {
+		-- open
+		["("] = { action = "open", pair = "()", neigh_pattern = "^[^\\]" },
+		["["] = { action = "open", pair = "[]", neigh_pattern = "^[^\\]" },
+		["{"] = { action = "open", pair = "{}", neigh_pattern = "^[^\\]" },
+
+		-- close
+		[")"] = { action = "close", pair = "()", neigh_pattern = "^[^\\]" },
+		["]"] = { action = "close", pair = "[]", neigh_pattern = "^[^\\]" },
+		["}"] = { action = "close", pair = "{}", neigh_pattern = "^[^\\]" },
+
+		['"'] = { action = "closeopen", pair = '""', neigh_pattern = "^[^\\]", register = { cr = false } },
+		["'"] = { action = "closeopen", pair = "''", neigh_pattern = "^[^%a\\]", register = { cr = false } },
+		["`"] = { action = "closeopen", pair = "``", neigh_pattern = "^[^\\]", register = { cr = false } },
+	},
+})
