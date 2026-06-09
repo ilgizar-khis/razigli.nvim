709a73cc5282d76f211165395ae1001f7ddd7371
razigli <razigli@arch.dev>
2026-06-09 18:09:08 +0500

    config conform.nvim:

--- lua/lsp/format.lua
+++ lua/lsp/format.lua
@@ -1,3 +1,21 @@
 vim.pack.add({
 	{ src = "https://github.com/stevearc/conform.nvim.git" },
 })
+
+local conform = require("conform")
+conform.setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		-- Conform will run multiple formatters sequentially
+		python = { "black" },
+		-- You can customize some of the format options for the filetype (:help conform.format)
+		rust = { "rustfmt", lsp_format = "fallback" },
+		json = { "prettier" },
+		qml = { "prettier" },
+		cpp = { "clang_format" },
+		c = { "clang_format" },
+		java = { "lsp", lsp_format = "fallback" },
+		-- Conform will run the first available formatter
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+	},
+})
