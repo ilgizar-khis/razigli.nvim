8b7d3ddac349c7483510fea8aff0961672d02a47
razigli <razigli@arch.dev>
2026-06-09 22:25:05 +0500

    add keumap to buffers and wins

--- init.lua
+++ init.lua
@@ -25,6 +25,20 @@ vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#ffcabc", bg = "none" })
 vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#ffcabc", bg = "none" })
 vim.api.nvim_set_hl(0, "SignColumn", { bg = "#000000" })
 
+-- KEYMAP
+-- tabs and buffers
+vim.keymap.set("n", "<M-h>", "<CMD>bprev<CR>", { noremap = true, silent = true }) -- to prev buffer
+vim.keymap.set("n", "<M-l>", "<CMD>bnext<CR>", { noremap = true, silent = true }) -- to next buffer
+-- wins
+vim.keymap.set("n", "<C-h>", "<CMD>wincmd h<CR>", { noremap = true, silent = true }) -- to left win
+vim.keymap.set("n", "<C-l>", "<CMD>wincmd l<CR>", { noremap = true, silent = true }) -- to right win
+vim.keymap.set("n", "<C-j>", "<CMD>wincmd j<CR>", { noremap = true, silent = true }) -- to down win
+vim.keymap.set("n", "<C-k>", "<CMD>wincmd k<CR>", { noremap = true, silent = true }) -- to up win
+vim.keymap.set("n", "<Tab>", "<CMD>wincmd w<CR>", { noremap = true, silent = true }) -- to next win
+vim.keymap.set("n", "<S-Tab>", "<CMD>wincmd W<CR>", { noremap = true, silent = true }) -- to next win
+vim.keymap.set("n", "\\", "<CMD>vsplit<CR>", { noremap = true, silent = true }) -- vertical split
+vim.keymap.set("n", "|", "<CMD>split<CR>", { noremap = true, silent = true }) -- horizontal split
+
 -- indents config
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
