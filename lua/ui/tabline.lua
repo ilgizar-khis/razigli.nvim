a374a49b4a8addac74c56710a48652a45cf67d15
razigli <razigli@arch.dev>
2026-06-09 15:30:32 +0500

    config tabline colors

--- lua/ui/tabline.lua
+++ lua/ui/tabline.lua
@@ -4,3 +4,14 @@ vim.pack.add({
 
 local MiniTabline = require("mini.tabline")
 MiniTabline.setup()
+
+local selected = {bg = "none", fg = "#ffffff", bold = true}
+local modified = {bg = "none", fg = "#ffffff"}
+local hl = {bg = "none", fg = "#ffcabc"}
+
+vim.api.nvim_set_hl(0, "MiniTablineCurrent", selected)
+vim.api.nvim_set_hl(0, "MiniTablineVisible", selected)
+vim.api.nvim_set_hl(0, "MiniTablineHidden", hl)
+vim.api.nvim_set_hl(0, "MiniTablineModifiedCurrent", selected)
+vim.api.nvim_set_hl(0, "MiniTablineModifiedVisible", selected)
+vim.api.nvim_set_hl(0, "MiniTablineModifiedHidden", modified)
