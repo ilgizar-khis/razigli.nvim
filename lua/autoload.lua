16b741a51399d5ee3323fdd64eee339aaa934730
razigli <razigli@arch.dev>
2026-06-09 15:05:52 +0500

    done autolaod.lua

--- lua/autoload.lua
+++ lua/autoload.lua
@@ -1,9 +1,9 @@
 local M = {}
 
-M.requires = {}
-M.preloads = {}
-M.postloads = {}
-M.ignores = {"autoload"}
+M.require = {}
+M.preload = {}
+M.postload = {}
+M.ignore = {["autoload"] = true}
 
 function M.scan(dirName)
 	local files = vim.fn.readdir(dirName)
@@ -16,11 +16,43 @@ function M.scan(dirName)
 			M.scan(path, reqName)
 		else
 			if string.find(path, "%.lua") then
-				table.insert(M.requires, path)
-				vim.notify(path)
+				table.insert(M.require, path)
 			end
 		end
 
+	end
+end
+
+function M.parse(file, path)
+	file = string.match(file, path .. "/(.+)%.lua")
+	file = string.gsub(file, "/", "%.")
+	return file
+end
+
+function M.load()
+	local filePath = debug.getinfo(1, "S").source:sub(2)
+	local path = string.match(filePath, "(.+)/autoload.lua")
+	M.scan(path)
+
+	for file, enabled in pairs(M.preload) do
+		if enabled then
+			require(file)
+		end
+	end
+
+	for _, file in ipairs(M.require) do
+		file = M.parse(file, path)
+		if M.preload[file] or M.postload[file] or M.ignore[file] then
+			goto continue
+		end
+		require(file)
+		::continue::
+	end
+
+	for file, enabled in pairs(M.postload) do
+		if enabled then
+			require(file)
+		end
 	end
 end
 
