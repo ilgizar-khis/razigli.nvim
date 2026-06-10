29334db78e1a6208599eedebb684dc292d22c8e4
razigli <razigli@arch.dev>
2026-06-10 17:54:02 +0500

    add hl to NeoTree

--- lua/ui/neotree.lua
+++ lua/ui/neotree.lua
@@ -1,14 +1,14 @@
 -- from oficial github repo
 vim.pack.add({
-  {
-    src = 'https://github.com/nvim-neo-tree/neo-tree.nvim',
-    version = vim.version.range('3')
-  },
-  -- dependencies
-  "https://github.com/nvim-lua/plenary.nvim",
-  "https://github.com/MunifTanjim/nui.nvim",
-  -- optional, but recommended
-  "https://github.com/nvim-tree/nvim-web-devicons",
+	{
+		src = "https://github.com/nvim-neo-tree/neo-tree.nvim",
+		version = vim.version.range("3"),
+	},
+	-- dependencies
+	"https://github.com/nvim-lua/plenary.nvim",
+	"https://github.com/MunifTanjim/nui.nvim",
+	-- optional, but recommended
+	"https://github.com/nvim-tree/nvim-web-devicons",
 })
 
 require("neo-tree").setup({
@@ -22,9 +22,14 @@ require("neo-tree").setup({
 			["L"] = "expand_all_subnodes",
 			["H"] = "set_root",
 			["."] = "toggle_hidden",
-		}
-	}
+		},
+	},
 })
 
 -- close and open keymap
-vim.keymap.set("n", "<S-e>", "<CMD>Neotree toggle<CR>", {noremap = true, silent = true})
+vim.keymap.set("n", "<S-e>", "<CMD>Neotree toggle<CR>", { noremap = true, silent = true })
+
+-- highlights
+-- vim.api.nvim_set_hl(0, "NeoTreeFilterTerm", {})
+vim.api.nvim_set_hl(0, "NeoTreeFloatBorder", { bg = "#565656", fg = "#111111" })
+vim.api.nvim_set_hl(0, "NeoTreeTitleBar", { bg = "#565656", fg = "#ffffff" })
