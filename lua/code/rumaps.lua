8c6fc02b7775143b86c959f85ac8a2afdc697852
razigli <razigli@arch.dev>
2026-06-18 11:55:49 +0500

    add comments to insert mode

--- lua/code/rumaps.lua
+++ lua/code/rumaps.lua
@@ -6,19 +6,18 @@ vim.keymap.set("n", "д", "l", { noremap = true, silent = true }) -- right
 vim.keymap.set("n", "пп", "gg", { noremap = true, silent = true }) -- start
 vim.keymap.set("n", "П", "G", { noremap = true, silent = true }) -- end
 
--- insert mode
+-- enter to insert mode
+vim.keymap.set("n", "ш", "i", { noremap = true, silent = true }) -- before the cursor
+vim.keymap.set("n", "ф", "a", { noremap = true, silent = true }) -- after the cursor
+vim.keymap.set("n", "ы", "s", { noremap = true, silent = true }) -- in the cursor
+vim.keymap.set("n", "щ", "o", { noremap = true, silent = true }) -- below the cursor
+vim.keymap.set("n", "с", "c", { noremap = true, silent = true }) -- change
 
-vim.keymap.set("n", "ш", "i", { noremap = true, silent = true })
-vim.keymap.set("n", "ф", "a", { noremap = true, silent = true })
-vim.keymap.set("n", "ы", "s", { noremap = true, silent = true })
-vim.keymap.set("n", "щ", "o", { noremap = true, silent = true })
-vim.keymap.set("n", "с", "c", { noremap = true, silent = true })
-
-vim.keymap.set("n", "Ш", "I", { noremap = true, silent = true })
-vim.keymap.set("n", "Ф", "A", { noremap = true, silent = true })
-vim.keymap.set("n", "Ы", "S", { noremap = true, silent = true })
-vim.keymap.set("n", "Щ", "O", { noremap = true, silent = true })
-vim.keymap.set("n", "С", "C", { noremap = true, silent = true })
+vim.keymap.set("n", "Ш", "I", { noremap = true, silent = true }) -- start of line
+vim.keymap.set("n", "Ф", "A", { noremap = true, silent = true }) -- end of line
+vim.keymap.set("n", "Ы", "S", { noremap = true, silent = true }) -- in the line
+vim.keymap.set("n", "Щ", "O", { noremap = true, silent = true }) -- above the cursor
+vim.keymap.set("n", "С", "C", { noremap = true, silent = true }) -- change line
 
 -- undo/redo
 
