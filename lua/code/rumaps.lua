d5ff98c50f2f178c7b00a0d06f51f63c1af654e5
razigli <razigli@arch.dev>
2026-06-18 11:37:45 +0500

    add keys to enter insert mode

---
+++ lua/code/rumaps.lua
@@ -0,0 +1,13 @@
+-- insert mode
+
+vim.keymap.set("n", "ш", "i", { noremap = true, silent = true })
+vim.keymap.set("n", "ф", "a", { noremap = true, silent = true })
+vim.keymap.set("n", "ы", "s", { noremap = true, silent = true })
+vim.keymap.set("n", "щ", "o", { noremap = true, silent = true })
+vim.keymap.set("n", "с", "c", { noremap = true, silent = true })
+
+vim.keymap.set("n", "Ш", "I", { noremap = true, silent = true })
+vim.keymap.set("n", "Ф", "A", { noremap = true, silent = true })
+vim.keymap.set("n", "Ы", "S", { noremap = true, silent = true })
+vim.keymap.set("n", "Щ", "O", { noremap = true, silent = true })
+vim.keymap.set("n", "С", "C", { noremap = true, silent = true })
