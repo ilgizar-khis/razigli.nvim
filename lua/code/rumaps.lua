1083b9c74a862480517202b87cad3e4df5f24a81
razigli <razigli@arch.dev>
2026-06-18 11:47:41 +0500

    add comments to movement

--- lua/code/rumaps.lua
+++ lua/code/rumaps.lua
@@ -1,11 +1,10 @@
--- movement
-
-vim.keymap.set("n", "р", "h", { noremap = true, silent = true })
-vim.keymap.set("n", "о", "j", { noremap = true, silent = true })
-vim.keymap.set("n", "л", "k", { noremap = true, silent = true })
-vim.keymap.set("n", "д", "l", { noremap = true, silent = true })
-vim.keymap.set("n", "пп", "gg", { noremap = true, silent = true })
-vim.keymap.set("n", "П", "G", { noremap = true, silent = true })
+-- move cursor to
+vim.keymap.set("n", "р", "h", { noremap = true, silent = true }) -- left
+vim.keymap.set("n", "о", "j", { noremap = true, silent = true }) -- down
+vim.keymap.set("n", "л", "k", { noremap = true, silent = true }) -- up
+vim.keymap.set("n", "д", "l", { noremap = true, silent = true }) -- right
+vim.keymap.set("n", "пп", "gg", { noremap = true, silent = true }) -- start
+vim.keymap.set("n", "П", "G", { noremap = true, silent = true }) -- end
 
 -- insert mode
 
