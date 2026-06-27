c638e228da9034eed8e67c60112c7c332e4d2109
razigli <razigli@arch.dev>
2026-06-27 12:49:28 +0500

    move HL from init.lua to core/hl.lua

--- init.lua
+++ init.lua
@@ -8,19 +8,6 @@ vim.opt.signcolumn = "yes:2"
 vim.opt.cursorline = false
 vim.opt.cursorcolumn = false
 
--- HL
--- basic
-vim.cmd("colorscheme sorbet")
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "WinSeparator", { bg = "#000000", fg = "#ffffff" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#121212" })
-vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#000000", fg = "#ffffff" })
--- cols
-vim.api.nvim_set_hl(0, "LineNr", { fg = "#ffffff", bg = "none" })
-vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#ffcabc", bg = "none" })
-vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#ffcabc", bg = "none" })
-vim.api.nvim_set_hl(0, "SignColumn", { bg = "#000000" })
-
 -- indents config
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
---
+++ lua/core/hl.lua
@@ -0,0 +1,15 @@
+-- HL
+-- basic
+vim.cmd("colorscheme sorbet")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "WinSeparator", { bg = "#000000", fg = "#ffffff" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#121212" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#000000", fg = "#ffffff" })
+-- cols
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#ffffff", bg = "none" })
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#ffcabc", bg = "none" })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#ffcabc", bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "#000000" })
+-- code
+vim.api.nvim_set_hl(0, "Comment", { fg = "#ababab", italic = true })
+vim.api.nvim_set_hl(0, "String", { fg = "#ffab00", italic = true })
