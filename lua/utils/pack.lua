8f8947116c802dff6eb6671d4decdc82f6de43ca
razigli <razigli@arch.dev>
2026-07-07 18:05:20 +0500

    fix errors

--- lua/utils/pack.lua
+++ lua/utils/pack.lua
@@ -1,11 +1,14 @@
 vim.pack.add({
 	{
 		src = "https://github.com/ilgizar-khis/packs.razigli.git",
+		version = "main",
 	},
 })
 
 local packs = require("packs.razigli")
-packs.setup()
+packs.setup({
+	width = 130,
+})
 
 vim.api.nvim_create_user_command("Packs", function()
 	packs.toggle_win()
