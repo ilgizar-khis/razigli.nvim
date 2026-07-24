2a971205f205f9ed05c96a277caff403a7504dbe
razigli <razigli@arch.dev>
2026-07-24 16:13:24 +0500

    add langd to autocmd

--- lua/lsp/treesitter.lua
+++ lua/lsp/treesitter.lua
@@ -1,7 +1,7 @@
 vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter.git" })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "python", "lua", "json" },
+	pattern = { "python", "lua", "json", "qml", "rust", "java" },
 	callback = function()
 		vim.treesitter.start()
 	end,
