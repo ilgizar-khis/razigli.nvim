8781988e42013e8540092b6dcac60982c57859c2
Razigli <ilgizarhisametdinov40@gmail.com>
2026-09-08 14:54:07 +0500

    add to prev bind

--- ftplugin/http.lua
+++ ftplugin/http.lua
@@ -34,3 +34,10 @@ vim.keymap.set("n", "J", function()
 	local i = find_header(lines, line_nr + 1, #lines) or find_header(lines, 1, line_nr)
 	vim.api.nvim_win_set_cursor(0, { i or 1, 0 })
 end)
+
+vim.keymap.set("n", "K", function()
+	local line_nr = vim.api.nvim_win_get_cursor(0)[1]
+	local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+	local i = find_header(lines, line_nr - 1, 1, true) or find_header(lines, #lines, line_nr, true)
+	vim.api.nvim_win_set_cursor(0, { i or 1, 0 })
+end)
